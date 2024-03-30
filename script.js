6ff8c06356c76c5e4b2b0faa56958cc233393cff
Md <mdtauseefkhan38@gmail.com>
2024-03-30 15:47:39 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById("createTeamBtn").addEventListener("click", function() {
+    alert("Create team button clicked");
+  });
+  
+  document.getElementById("meetingBtn").addEventListener("click", function() {
+    alert("Meeting button clicked");
+  });
+  
+  document.getElementById("importExportBtn").addEventListener("click", function() {
+    alert("Import/Export button clicked");
+  });
+  
+  document.getElementById("inviteBtn").addEventListener("click", function() {
+    alert("Invite teammates button clicked");
+  });
+  
+  document.getElementById("trialBtn").addEventListener("click", function() {
+    alert("7 days left on trial button clicked");
+  });
+  
+  document.getElementById("billingBtn").addEventListener("click", function() {
+    alert("Add billing button clicked");
+  });
+
+ 
